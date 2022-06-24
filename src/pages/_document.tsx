374c11d013cad5e07db060ddbe679df01c41cf7e
h64 <1981460+h64@users.noreply.github.com>
2022-06-24 12:31:34 -0700

    Remove console warning through using different _document.tsx example

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ import Document, {
 
 import { ServerStyleSheet } from "styled-components";
 
-// https://github.com/vercel/next.js/blob/canary/examples/with-styled-components-babel/pages/_document.tsx
+// https://github.com/vercel/next.js/blob/canary/examples/with-styled-components/pages/_document.tsx
 class MyDocument extends Document {
   render() {
     return (
@@ -39,12 +39,7 @@ class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: [
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>,
-        ],
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
